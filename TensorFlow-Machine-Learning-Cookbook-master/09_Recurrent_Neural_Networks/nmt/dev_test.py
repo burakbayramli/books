282557ee2c34d68c45be5c7c9db5a9c88bbb7e50
Burak Bayramli <burak@localhost.com>
2018-01-19 14:40:41 +0100

    -

---
+++ TensorFlow-Machine-Learning-Cookbook-master/09_Recurrent_Neural_Networks/nmt/dev_test.py
@@ -0,0 +1,30 @@
+fin = open ("/tmp/train.en")
+fout1 = open("/home/burak/Downloads/tur-eng/train.en","w")
+fout2 = open("/home/burak/Downloads/tur-eng/tst2012.en","w")
+fout3 = open("/home/burak/Downloads/tur-eng/tst2013.en","w")
+for i,line in enumerate(fin.readlines()):
+    if i < 20000:
+        fout1.write(line)
+        fout1.flush()
+    elif i < 22000:
+        fout2.write(line)
+        fout2.flush()
+    else:        
+        fout3.write(line)
+        fout3.flush()
+    
+fin = open ("/tmp/train.tr")
+fout1 = open("/home/burak/Downloads/tur-eng/train.tr","w")
+fout2 = open("/home/burak/Downloads/tur-eng/tst2012.tr","w")
+fout3 = open("/home/burak/Downloads/tur-eng/tst2013.tr","w")
+for i,line in enumerate(fin.readlines()):
+    if i < 20000:
+        fout1.write(line)
+        fout1.flush()
+    elif i < 22000:
+        fout2.write(line)
+        fout2.flush()
+    else:        
+        fout3.write(line)
+        fout3.flush()
+    
