ffd4be964564ca517601261109b547471322f5e3
Burak Bayramli <burak@localhost.com>
2018-01-21 23:09:52 +0100

    -

--- TensorFlow-Machine-Learning-Cookbook-master/09_Recurrent_Neural_Networks/nmthard/doall.sh
+++ TensorFlow-Machine-Learning-Cookbook-master/09_Recurrent_Neural_Networks/nmthard/doall.sh
@@ -1,5 +1,4 @@
-python -u split_vocab_manythings.py
-python -u remove_space.py
+python -u split_vocab.py
+sort -u /tmp/vocab.en > /home/burak/Downloads/tur-eng/vocab.en
+sort -u /tmp/vocab.tr > /home/burak/Downloads/tur-eng/vocab.tr
 python -u dev_test.py
-sort -u /tmp/vocab.2.en > /home/burak/Downloads/tur-eng/vocab.en
-sort -u /tmp/vocab.2.tr > /home/burak/Downloads/tur-eng/vocab.tr
