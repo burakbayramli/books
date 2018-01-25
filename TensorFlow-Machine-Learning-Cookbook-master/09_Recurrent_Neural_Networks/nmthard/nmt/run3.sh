11e07317e06c432c1e4ca6da1fd98d2bed3cf17c
Burak Bayramli <burak@localhost.com>
2018-01-25 13:43:54 +0100

    -

--- TensorFlow-Machine-Learning-Cookbook-master/09_Recurrent_Neural_Networks/nmthard/nmt/run3.sh
+++
@@ -1,8 +0,0 @@
-python -m nmt.nmt \
-    --src=de --tgt=en \
-    --hparams_path=nmt/standard_hparams/wmt16_gnmt_4_layer.json \
-    --out_dir=/tmp/deen_gnmt \
-    --vocab_prefix=/tmp/wmt16/vocab.bpe.32000 \
-    --train_prefix=/tmp/wmt16/train.tok.clean.bpe.32000 \
-    --dev_prefix=/tmp/wmt16/newstest2013.tok.bpe.32000 \
-    --test_prefix=/tmp/wmt16/newstest2015.tok.bpe.32000
