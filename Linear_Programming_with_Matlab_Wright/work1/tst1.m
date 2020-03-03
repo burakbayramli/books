47cf82affb4df340876062db7ccb5d407ee7d31f
BB <burak@localhost.com>
2020-03-03 10:28:16 +0300

    -

--- Linear_Programming_with_Matlab_Wright/work1/tst1.m
+++ Linear_Programming_with_Matlab_Wright/work1/tst1.m
@@ -1,10 +1,9 @@
 % generate a sparse random matrix of given density
 %m=10; n=100;
-m=3; n=4;
+m=2; n=4;
 %density=0.2;
 #A = sprandn(m,n,density);
-A = [-1 -5 0 0 ;
-      1  1 1 0 ;
+A = [ 1  1 1 0 ;
       1  3 0 1 ]
 
 A = sparse(A);
@@ -16,11 +15,14 @@ xfeas = xfeas(randperm(n)); sfeas = sfeas(randperm(n));
 yfeas = (rand(m,1)-0.5)*4;
 
 % choose b and p to make this (x,y,s) feasible
-b = A*xfeas; p=A'*yfeas+sfeas;
+%b = A*xfeas;
+b = [5;7]
+%p=A'*yfeas+sfeas;
+p = [-1; -5; 0; 0 ]
 
 % call the solver
-%[x,y,s,f] = pathfollow(A,b,p);
-[x,y,s,f] = pdip(A,b,p);
+[x,y,s,f] = pathfollow(A,b,p);
+%[x,y,s,f] = pdip(A,b,p);
 
 fprintf(1,' final primal value: %12.6e \n', p'*x);
 fprintf(1,' final dual   value: %12.6e \n', b'*y);
@@ -28,3 +30,11 @@ fprintf(1,' primal infeas: %12.6e \n', norm(A*x-b));
 fprintf(1,' dual   infeas: %12.6e \n', norm(A'*y+s-p));
 
 x
+
+%x =
+%   1.2677e-07
+%   2.3333e+00
+%   2.6667e+00
+%   5.0417e-08
+
+
