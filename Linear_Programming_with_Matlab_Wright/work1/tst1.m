258ef579ebd54d0b80d79c98347f2c081376304a
BB <burak@localhost.com>
2020-03-03 10:10:20 +0300

    -

--- Linear_Programming_with_Matlab_Wright/work1/tst1.m
+++ Linear_Programming_with_Matlab_Wright/work1/tst1.m
@@ -1,5 +1,13 @@
 % generate a sparse random matrix of given density
-m=10; n=100; density=0.2; A = sprandn(m,n,density);
+%m=10; n=100;
+m=3; n=4;
+%density=0.2;
+#A = sprandn(m,n,density);
+A = [-1 -5 0 0 ;
+      1  1 1 0 ;
+      1  3 0 1 ]
+
+A = sparse(A);
 
 % choose feasible x, y, s at random, with x and s each about half-full
 xfeas = [rand(n/2,1); zeros(n-(n/2),1)];
@@ -19,3 +27,4 @@ fprintf(1,' final dual   value: %12.6e \n', b'*y);
 fprintf(1,' primal infeas: %12.6e \n', norm(A*x-b));
 fprintf(1,' dual   infeas: %12.6e \n', norm(A'*y+s-p));
 
+x
