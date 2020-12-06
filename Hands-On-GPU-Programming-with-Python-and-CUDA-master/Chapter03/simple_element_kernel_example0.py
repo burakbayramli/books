405a1eb5fb5a51dc3548f6c8e315b386482dac00
MK <me@yomama.com>
2020-12-06 11:17:04 +0300

    -

--- Hands-On-GPU-Programming-with-Python-and-CUDA-master/Chapter03/simple_element_kernel_example0.py
+++ Hands-On-GPU-Programming-with-Python-and-CUDA-master/Chapter03/simple_element_kernel_example0.py
@@ -15,7 +15,7 @@ def speedcomparison():
     t1 = time()
     host_data_2x =  host_data * np.float32(2)
     t2 = time()
-    print 'total time to compute on CPU: %f' % (t2 - t1)
+    print ('total time to compute on CPU: %f' % (t2 - t1))
     device_data = gpuarray.to_gpu(host_data)
     # allocate memory for output
     device_data_2x = gpuarray.empty_like(device_data)
@@ -23,8 +23,8 @@ def speedcomparison():
     gpu_2x_ker(device_data, device_data_2x)
     t2 = time()
     from_device = device_data_2x.get()
-    print 'total time to compute on GPU: %f' % (t2 - t1)
-    print 'Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) )
+    print ('total time to compute on GPU: %f' % (t2 - t1))
+    print ('Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) ))
     
 
 if __name__ == '__main__':
