7ee72bf49096eadc6c8cc2f729c85ce9d4563242
MK <me@yomama.com>
2021-02-19 10:10:15 +0300

    -

--- Numerical Solution of Boundary Value Problems for Ordinary Differential Equations Ascher/MUSN.m
+++
@@ -1,6 +0,0 @@
-function [x,y] = MUSN
-% Import solution of MUSN
-
-temp = load('MUSN.dat');
-x = temp(:,1);
-y = temp(:,2:end);
--- Numerical Solution of Boundary Value Problems for Ordinary Differential Equations Ascher/swirl_III.m
+++
@@ -1,6 +0,0 @@
-function [x,y] = swirl_III
-% Import solution of swirl_III
-
-temp = load('swirl_III.dat');
-x = temp(:,1);
-y = temp(:,2:end);
