def44d0ce24a7b227c88a15eb809fce2984c4d59
MK <me@yomama.com>
2021-06-09 17:37:02 +0300

    -

--- build.py
+++ build.py
@@ -1,4 +1,11 @@
-import os
+import os, sys, glob
 
-os.system("zip /tmp/dotbooks.zip -r /home/burak/Documents/books/.git/")
+if sys.argv[1] == 'dot':
+    os.system("zip /tmp/dotbooks.zip -r /home/burak/Documents/books/.git/")
+if sys.argv[1] == 'title':
+    files = glob.glob("*")
+    for f in files:
+        fname = f.replace("_"," ")
+        fname = fname.replace("-"," ")
+        print ("- [%s](%s)" % (fname,f))
 
