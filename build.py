1b510c63e89ce84e1bee1aff8037130b0374c44f
MK <me@yomama.com>
2021-07-12 12:18:09 +0300

    -

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import os, sys, glob
 
 if sys.argv[1] == 'dot':
     os.system("zip /tmp/dotbooks.zip -r /home/burak/Documents/books/.git/")
+    
 if sys.argv[1] == 'title':
     files = glob.glob("*")
     for f in files:
