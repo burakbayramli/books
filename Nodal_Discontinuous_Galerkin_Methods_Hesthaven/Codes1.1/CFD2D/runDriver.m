193cf944302c63bd6b7c317e441f1fd5d0b14f47
BB <me@yomama.com>
2021-08-18 12:36:45 +0300

    -

--- Nodal_Discontinuous_Galerkin_Methods_Hesthaven/Codes1.1/CFD2D/runDriver.m
+++ Nodal_Discontinuous_Galerkin_Methods_Hesthaven/Codes1.1/CFD2D/runDriver.m
@@ -1,3 +1,6 @@
 % Run this on Octave using 'octave runDriver.m'
 addpath('../Codes2D:../Grid/Euler2D:../Codes1D:../ServiceRoutines');
 EulerDriver2D;
+figure; PlotField2D(N, x, y, Q(:,:,4)); drawnow; pause(.1);
+%page_screen_output(0);
+%page_output_immediately(1);
