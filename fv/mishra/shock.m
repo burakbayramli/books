2d80daa21248cc12c2442dcc4aeb4bc309a0d083
BB <me@yomama.com>
2022-01-14 14:53:51 +0300

    -

--- fv/mishra/shock.m
+++
@@ -1,3 +0,0 @@
-function [ u0 ] = shock()
-  u0 = @(x) x < 0;
-end
--- fv/mishra/solve_all.m
+++
@@ -1,5 +0,0 @@
-function [ x, u, dx] = solve_all( f, f_prime, f_type,f_extrema, u0, l, r, T, mesh_size )
-  % Godunov scheme
-  flux = Godunov( f, f_prime, f_type, f_extrema);
-  [x, u(1,:), dx] = solve(f_prime, flux, u0, l, r, T,mesh_size);
-  [~, u(2,:),~] = solve(f_prime, flux, u0, l, r, T, mesh_size);
