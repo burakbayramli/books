5ed5f8e363c5dcf5fdd347e7c18d427247df77f9
BB <me@yomama.com>
2022-05-27 12:53:02 +0300

    -

--- Riemann_Problems_and_Jupyter_Solutions_Leveque/run_sod_roe.py
+++ Riemann_Problems_and_Jupyter_Solutions_Leveque/run_sod_roe.py
@@ -10,6 +10,9 @@ def primitive_to_conservative(rho, u, p, gamma=1.4):
     E   = p/(gamma-1.) + 0.5*rho*u**2
     return rho, mom, E
 
+def pospart(x):
+    return np.maximum(1.e-15,x)
+
 def conservative_to_primitive(rho, mom, E, gamma=1.4):
     u = mom/pospart(rho)
     p = (gamma-1.)*(E - 0.5*rho*u**2)
@@ -75,13 +78,14 @@ def Euler_roe(q_l, q_r, gamma=1.4):
     
     xmax = 0.9
     x = np.linspace(-xmax, xmax, 100)
-    t = 0.6 # change this value to see graph at different time points
+    t = 0.4 # change this value to see graph at different time points
     q = reval(x/(t+1e-10))
+    q = cons_to_prim(q)
 
     fig, axes = plt.subplots(3, 1, figsize=(5, 6), sharex=True)
-    axes[0].plot(x,q[0])
-    axes[1].plot(x,q[1])
-    axes[2].plot(x,q[2])
+    axes[0].plot(x,q[0]); axes[0].set_ylim(0,4)
+    axes[1].plot(x,q[1]); axes[0].set_ylim(0,1)
+    axes[2].plot(x,q[2]); axes[0].set_ylim(0,4)
     
     plt.savefig('/tmp/sod-out.png')
 
