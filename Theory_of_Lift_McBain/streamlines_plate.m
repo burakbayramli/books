b1ca02d1c5954684b817e8a40aac5fa6e8a8c3f6
BB <me@yomama.com>
2024-03-18 08:12:25 +0300

    -

--- Theory_of_Lift_McBain/streamlines_plate.m
+++ Theory_of_Lift_McBain/streamlines_plate.m
@@ -1,3 +1,4 @@
+fig = figure;
 alpha = pi/6; c = 1; Gamma = c * pi * sin (alpha);
 W = @ (zeta) zeta + (c/4)^2 ./ zeta;
 W1 = @ (zeta) W (zeta) - Gamma * log (4 * zeta / c) / pi / 2i;
@@ -6,3 +7,4 @@ W3 = @ (z) W2 (joukowsky_inverse (z, c));
 streamlines_W (-2-2i, 2+2i, W3, -3:0.1:4 )
 hold ('on')
 plot ([0, 1], [0, 0], 'LineWidth', 3, 'LineStyle', '-')
+print(fig,'/tmp/plate.png','-dpng')
