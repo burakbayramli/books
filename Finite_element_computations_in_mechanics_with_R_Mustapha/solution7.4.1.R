e9cdfa15e0a211dfeb4d0f1849c87ace8f2a822a
BB <me@yomama.com>
2024-11-29 09:33:13 +0300

    -

---
+++ Finite_element_computations_in_mechanics_with_R_Mustapha/solution7.4.1.R
@@ -0,0 +1,6 @@
+source ("PlanarFrameElement.R");
+E1=E2=210e9;A1=A2=2.1;I1=I2=300e-6
+L1T1=PlaneFrame_LengthOrientation(c(0,4,5,4));
+L2T2=PlaneFrame_LengthOrientation(c(5,4,5,0));
+L1T1=PlaneFrame_LengthOrientation(c(0,4,5,4));L1T1
+L2T2=PlaneFrame_LengthOrientation(c(5,4,5,0));L2T2
