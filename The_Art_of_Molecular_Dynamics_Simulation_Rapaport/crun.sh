a20858e606b350a6296d9243e3213e8c4194a1a9
BB <me@yomama.com>
2025-05-08 10:56:43 +0300

    -

--- The_Art_of_Molecular_Dynamics_Simulation_Rapaport/crun.sh
+++ The_Art_of_Molecular_Dynamics_Simulation_Rapaport/crun.sh
@@ -1,10 +1,8 @@
 #! /bin/tcsh
-# install tcsh if missing, 'sudo apt install tcsh'
-# for pr_08_1.c example, run with 'tcsh crun.sh 08 1'
 set f=src/pr_$1_$2
 echo "$f"
 set b=`basename $f .c`
 echo "$b"
 gcc -O -I./src -o $b src/$b.c -lm
-./$b data/$b.in $b.in
+cp src/$b.in $b.in
 ./$b | tee $b.out
