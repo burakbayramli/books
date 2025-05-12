80e66c4dfa77ff047646a6f0cbdef8ac49eca92f
BB <me@yomama.com>
2025-05-12 12:00:34 +0300

    -

--- Game_Physics_Eberly/WildMagic2/Source/make.sh
+++
@@ -1,2 +0,0 @@
-INCDIR="-I./Approximation -I./Containment -I./Curves -I./Distance -I./Geometry -I./Graphics -I./ImageAnalysis -I./Interpolation -I./Intersection -I./Math -I./Meshes -I./Miscellaneous -I./Numerics -I./Physics -I./RationalArithmetic -I./Surfaces -I./System -I./Tessellation"
-g++ $INCDIR -c -o Physics/WmlRigidBody.o Physics/WmlRigidBody.cpp 
